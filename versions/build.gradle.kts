bfbf7baf7d884ffd2842e3c7bbdfc99f596dd17f
Christoph <siedlerkiller@gmail.com>
2025-11-25 20:47:22 +0100

    Update versions/build.gradle.kts
    Co-authored-by: Jendrik Johannes <jendrik.johannes@gmail.com>

--- versions/build.gradle.kts
+++ versions/build.gradle.kts
@@ -18,7 +18,8 @@ dependencies.constraints {
     api("org.openjfx:javafx-base:$javafx")
    // api("org.openjfx:javafx-controls:$javafx")
     // api("org.openjfx:javafx-fxml:$javafx")
-    // api("org.openjfx:javafx-graphics:${javafx}")
+    api("org.openjfx:javafx-graphics:${javafx}")
+    api("org.controlsfx:controlsfx:11.2.2")
    // api("org.openjfx:javafx-swing:$javafx")
   //  api("org.openjfx:javafx-web:$javafx")
     // from JavaFX25 onwards
